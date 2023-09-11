2a1ac0ab9dcc573d3e6e6f001c435b7a1963e48d
Junyu <83873214+junyu0312@users.noreply.github.com>
2023-09-11 18:20:19 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
-pub fn rlp() -> i64 {
+pub fn zkmain() -> i64 {
     let data = vec![0x83, b'c', b'a', b't'];
     let _animal: String = rlp::decode(&data).unwrap();
     // assert_eq!(animal, "cat".to_owned());
